e3a9e2446fa6978d2e3b7c3a70172e12dfa42b5c
ygladiensyah <ygladiensyah@gmail.com>
2018-11-21 08:47:42 +0700

    first commit

--- python/biologi.py
+++ python/biologi.py
@@ -1,9 +1,11 @@
 biologi = []
-a = 0
+a = 1
+i = 0
 jalan = True
 
 while(jalan):
     tanya = input("Apakah ingin menginput data? (y/t): ")
+    print("-----------------------------")
     if(tanya == "y"):
         data_biologi = input ("Input nama genus dan spesies tanaman ke-{}: ".format(a))
         biologi.append(data_biologi)
@@ -11,10 +13,15 @@ while(jalan):
         print ("Data yang telah tersimpan sebanyak {} tanaman".format(len(biologi)))
         for bio in biologi:
             print ("- {}".format(bio))
+        print("-----------------------------")    
     elif(tanya == "t"):
         tanya1 = input("Apakah ingin mencari data? (y/t): ")
+        print("-----------------------------")
         if(tanya1 == "y"):
             masuk = input("Masukkan nama spesies dari tanaman: ")
-            for masuk in biologi:
-                print("Data Ditemukan!")
+            for masuk in biologi.index(i):
+                print("Data Ditemukan")
                 print(masuk)
+                i+=1
+        if(tanya1 == "t"):
+            jalan = False
